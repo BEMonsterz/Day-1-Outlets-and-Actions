fda6cb1253314eea02c01de140e2d91b4119b861
Bryan Ayllon <bryanayllonroa@gmail.com>
2016-06-13 14:40:37 -0500

    First Homework

--- Day 1 Outlets and Actions/ViewController.m
+++ Day 1 Outlets and Actions/ViewController.m
@@ -9,6 +9,13 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property(nonatomic,weak) IBOutlet UITextField *firstNameTextField;
+@property(nonatomic,weak) IBOutlet UITextField *lastNameTextField;
+
+
+@property(nonatomic,weak) IBOutlet UILabel *firstNameLabel;
+@property(nonatomic,weak) IBOutlet UILabel *lastNameLabel;
+
 
 @end
 
@@ -17,6 +24,24 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+-(IBAction) submitButtonClicked{
+    
+    NSString *firstName = self.firstNameTextField.text;
+    NSString *lastName = self.lastNameTextField.text;
+    
+    NSString *welcomeMessage = [NSString stringWithFormat:@"%@ %@",firstName,lastName];
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Message" message:welcomeMessage preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+    }]];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+
+
+
 }
 
 - (void)didReceiveMemoryWarning {
